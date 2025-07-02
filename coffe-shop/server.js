0841d1ccc5024e1fff99bc51969f1418b09de9ff
Julius <flowinthedream@gmail.com>
2025-07-02 13:06:59 +0800

    Update

--- coffe-shop/server.js
+++ coffe-shop/server.js
@@ -93,7 +93,7 @@ const storeHours = {
 };
 
 // Email transporter setup
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
